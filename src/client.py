0764ae30013291ad752660a6b407586981b68617
evgenysobko <jooohn127@gmail.com>
2021-01-20 20:17:47 +0300

    Initial commit

---
+++ src/client.py
@@ -0,0 +1,43 @@
+import os
+import socket
+import threading
+import random
+
+
+def receive_data(sock):
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            print(data.decode('utf-8'))
+        except:
+            pass
+
+
+def run_client(ip):
+    host = socket.gethostbyname("0.0.0.0")
+    port = random.randint(6000, 10000)
+    server = (str(ip), 9999)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    name = input('Please write your name here: ')
+    if name == '':
+        name = 'Guest' + str(random.randint(1000, 9999))
+        print('Your name is:' + name)
+    s.sendto(name.encode('utf-8'), server)
+    threading.Thread(target=receive_data, args=(s,)).start()
+    while True:
+        data = input()
+        if data == 'qqq':
+            break
+        elif data == '':
+            continue
+        data = '[' + name + ']' + '->' + data
+        s.sendto(data.encode('utf-8'), server)
+    s.sendto(data.encode('utf-8'), server)
+    s.close()
+    os._exit(1)
+
+
+if __name__ == '__main__':
+    run_client('localhost')
---
+++ src/server.py
@@ -0,0 +1,46 @@
+import queue
+import threading
+import socket
+
+
+def recv_data(sock, recv_packets):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        recv_packets.put((data, addr))
+
+
+def run_server():
+    host = socket.gethostbyname("0.0.0.0")
+    port = 9999
+    print('Server hosting on IP-> ' + str(host))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    clients = set()
+    recvPackets = queue.Queue()
+
+    print('Server Running...')
+
+    threading.Thread(target=recv_data, args=(s, recvPackets)).start()
+
+    while True:
+        while not recvPackets.empty():
+            data, addr = recvPackets.get()
+            if addr not in clients:
+                clients.add(addr)
+                continue
+            clients.add(addr)
+            data = data.decode('utf-8')
+            if data.endswith('qqq'):
+                clients.remove(addr)
+                continue
+            print(str(addr) + data)
+            for c in clients:
+                if c != addr:
+                    s.sendto(data.encode('utf-8'), c)
+    s.close()
+
+
+if __name__ == '__main__':
+    run_server()
+    print('Server is online now!')
+
